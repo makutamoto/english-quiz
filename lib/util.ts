f19706915b8be1821269d7675b47f166617e1ec5
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-08-22 16:36:59 +0900

    implemented #9

--- lib/util.ts
+++ lib/util.ts
@@ -1,5 +1,8 @@
+const REGEX_BACKTICK = /’/g
+
 export function normalizeString(x: string) {
   const words: string[] = []
+  x = x.replace(REGEX_BACKTICK, "'")
   x.split(' ').forEach((word) => {
     if (word !== '') words.push(word)
   })
